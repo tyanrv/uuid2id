159d9943f9bb645a29d4a31ae3a2fbf0be24ae20
tyanrv <tyanrv@gmail.com>
2020-12-03 15:27:19 +0600

    create Container throw ContainerBuilder

--- api/public/index.php
+++ api/public/index.php
@@ -2,20 +2,28 @@
 
 declare(strict_types=1);
 
+use DI\ContainerBuilder;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use Slim\Factory\AppFactory;
-use DI\Container;
 
 http_response_code(500);
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$container = new Container();
+$builder = new ContainerBuilder();
 
-$app = AppFactory::create();
+$builder->addDefinitions([
+    'config' => [
+        'debug' => (bool)getenv('APP_DEBUG')
+    ]
+]);
 
-$app->addErrorMiddleware((bool) getenv('APP_DEBUG'), true, true);
+$container = $builder->build();
+
+$app = AppFactory::createFromContainer($container);
+
+$app->addErrorMiddleware($container->get('config')['debug'], true, true);
 
 $app->get('/', function (RequestInterface $request, ResponseInterface $response, $args) {
     $response->getBody()->write('{}');
