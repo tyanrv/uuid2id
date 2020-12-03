51538620d24e7bdfbed332f46e7bcab80e24548e
tyanrv <tyanrv@gmail.com>
2020-12-03 16:48:34 +0600

    make my console commands

--- api/bin/app.php
+++ api/bin/app.php
@@ -2,13 +2,20 @@
 
 declare(strict_types=1);
 
-use App\Console\HelloCommand;
+use Psr\Container\ContainerInterface;
 use Symfony\Component\Console\Application;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+/** @var ContainerInterface $container */
+$container = require_once __DIR__ . '/../config/container.php';
+
 $cli = new Application('Console');
 
-$cli->add(new HelloCommand());
+$commands = $container->get('config')['console']['commands'];
+
+foreach ($commands as $command) {
+    $cli->add($container->get($command));
+}
 
 $cli->run();
