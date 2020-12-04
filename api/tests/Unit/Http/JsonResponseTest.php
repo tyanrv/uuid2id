3eae580faff415c2d1f99f8d45b32841c78b86f7
tyanrv <tyanrv@gmail.com>
2020-12-04 17:57:07 +0600

    tests: cover JsonResponse

---
+++ api/tests/Unit/Http/JsonResponseTest.php
@@ -0,0 +1,72 @@
+<?php
+
+
+namespace Test\Unit\Http;
+
+
+use App\Http\JsonResponse;
+use PHPUnit\Framework\TestCase;
+use stdClass;
+
+/**
+ * Class JsonResponseTest
+ * @package Test\Unit\Http
+ * @covers JsonResponse
+ */
+class JsonResponseTest extends TestCase
+{
+    public function testInt(): void
+    {
+        $response = new JsonResponse(12);
+
+        self::assertEquals('12', $response->getBody()->getContents());
+        self::assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testIntWithCode(): void
+    {
+        $response = new JsonResponse(12, 201);
+
+        self::assertEquals('12', $response->getBody()->getContents());
+        self::assertEquals(201, $response->getStatusCode());
+    }
+
+    public function testNull(): void
+    {
+        $response = new JsonResponse(null);
+
+        self::assertEquals('null', $response->getBody()->getContents());
+        self::assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testString(): void
+    {
+        $response = new JsonResponse('12');
+
+        self::assertEquals('"12"', $response->getBody()->getContents());
+        self::assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testObject(): void
+    {
+        $object = new stdClass();
+        $object->str = 'value';
+        $object->int = 1;
+        $object->none = null;
+
+        $response = new JsonResponse($object);
+
+        self::assertEquals('{"str":"value","int":1,"none":null}', $response->getBody()->getContents());
+        self::assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testArray(): void
+    {
+        $array = ['str' => 'value', 'int' => 1, 'none' => null];
+
+        $response = new JsonResponse($array);
+
+        self::assertEquals('{"str":"value","int":1,"none":null}', $response->getBody()->getContents());
+        self::assertEquals(200, $response->getStatusCode());
+    }
+}
